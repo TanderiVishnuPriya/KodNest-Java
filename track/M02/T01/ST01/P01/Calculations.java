b98f240c3505e5257215190cf6f7e269a12fb0bd
Vishnu Priya <priyapriya125sss@gmail.com>
2026-08-04 11:58:44 +0530

    changes of code

---
+++ track/M02/T01/ST01/P01/Calculations.java
@@ -0,0 +1,19 @@
+public class Calculations {
+    public static void main(String[] args) {
+        double principal = 10000.0;
+        double rate = 6.5;
+        double time = 2.0;
+        double weight = 72.0;
+        double height = 1.8;
+        int total = 78 + 84 + 69 + 91 + 88;
+        double simpleInterest = principal * rate * time / 100;
+        double totalAmount = principal + simpleInterest;
+        double bmi = weight / (height * height);
+        double percentage = total * 100.0 / 500;
+        System.out.println("Simple Interest: " + simpleInterest);
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("BMI: " + bmi);
+        System.out.println("Total Marks: " + total);
+        System.out.println("Percentage: " + percentage);
+    }
+}
