dbefa6cdd4ae4dc7a808f5767003d591b9150262
Vishnu Priya <priyapriya125sss@gmail.com>
2026-08-14 11:50:00 +0530

    First Constructor

---
+++ track/M02/T06/FirstConstructor.java
@@ -0,0 +1,15 @@
+class Robot1 {
+    Robot1() {
+        // Print the message
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+
+    }
+
+}
+
+public class FirstConstructor {
+    public static void main(String[] args) {
+        // Create one Robot object
+        Robot1 r = new Robot1();
+    }
+}
