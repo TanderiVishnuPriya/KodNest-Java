7aedfa7ec7f23b41ccb2f39e5941c2b23c36dc17
Vishnu Priya <priyapriya125sss@gmail.com>
2026-08-18 11:15:00 +0530

    Array

---
+++ track/Arrays/ArrayAvg.java
@@ -0,0 +1,13 @@
+public class ArrayAvg {
+    public static void main(String[] args) {
+        // Write your code here.
+        int[] marks = { 70, 80, 60, 90, 50 };
+        int total = 0;
+        for (int i = 0; i < marks.length; i++) {
+            total += marks[i];
+        }
+        System.out.println("Total: " + total);
+        double avg = (double) total / marks.length;
+        System.out.println("Average: " + avg);
+    }
+}
