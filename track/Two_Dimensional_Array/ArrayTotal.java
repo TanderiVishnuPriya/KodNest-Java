ed298504cdbea8327449921da35a95bbdff1991c
Vishnu Priya <priyapriya125sss@gmail.com>
2026-08-19 13:17:30 +0530

    Two-Dimensional Array

---
+++ track/Two_Dimensional_Array/ArrayTotal.java
@@ -0,0 +1,25 @@
+package track.Two_Dimensional_Array;
+
+import java.util.Scanner;
+
+public class ArrayTotal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int a[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            int total = 0;
+            for (int j = 0; j < col; j++) {
+                total += a[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " total: " + total);
+        }
+        sc.close();
+    }
+}
